e5c9328437e9009635ff4ee62c83903c4a8674b1
valennnnnndo <valenjacobdo07@gmail.com>
2026-05-13 20:26:33 -0300

    updaate

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-var cant = [1, 1];
-var nombres = ["Termo", "Mate"];
+const alumnos = ["val", "milton", "juana", "maria", "enzo"];
 
-function cambiar(i, d) {
-  if (cant[i] + d >= 1) cant[i] += d;
-  document.getElementById("qty-" + i).textContent = cant[i];
-}
+console.log(alumnos[0]);
+console.log(alumnos[1]);
+console.log(alumnos[2]);
+console.log(alumnos[3]);
+console.log(alumnos[4]);
 
-function comprar(i) {
-  document.getElementById("mensaje").textContent =
-    "Compraste " + cant[i] + " " + nombres[i] + "s";
+console.log(alumnos[0]);
+console.log(alumnos[4]);
+console.log(alumnos.length);
+
+alumnos[3] = "jose";
+
+alumnos.pop();
+alumnos.shift();
+alumnos.unshift("pablo");
+
+alumnos.push("camila");
+
+for (let index = 0; index < alumnos.length; index++) {
+  const alu = alumnos[index];
+  console.log(alumnos);
 }
+console.log(alumnos.includes("maria"));
+console.log(alumnos.indexOf("milton"));
